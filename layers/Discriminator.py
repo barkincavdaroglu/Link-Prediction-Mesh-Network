16c77d756e9f4c144909bf2bdaf8b7e39b450bac
BC <barkincavdar@gmail.com>
2022-12-24 17:17:03 +0300

    implemented initial version of discriminator

--- layers/Discriminator.py
+++ layers/Discriminator.py
@@ -4,14 +4,19 @@ import torch.nn as nn
 
 class Discriminator(nn.Module):
     def __init__(self, input_size, hidden_size):
+        """
+        Args:
+            input_size: Dimension of input
+            hidden_size: Dimension of hidden layer
+        """
         super(Discriminator, self).__init__()
         self.fc1 = nn.Sequential(nn.Linear(input_size, hidden_size), nn.Sigmoid())
         self.fc2 = nn.Sequential(nn.Linear(hidden_size, 1), nn.Sigmoid())
 
-    def forward(self, input):
+    def forward(self, input) -> torch.Tensor:
         """
-        :param input: FloatTensor (batch_size, input_size)
-        :return: FloatTensor (batch_size,)
+        Args:
+            input: Output of Generator, represented via row-wise adjacency matrix
         """
         output = self.fc2(self.fc1(input)).squeeze(-1)
         return output
