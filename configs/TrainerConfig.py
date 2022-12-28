c717ad0daced6f5a38cf40baf1dfd0eefda2c097
BC <barkincavdar@gmail.com>
2022-12-29 00:23:37 +0300

    wrapper classes for model configs and training parameters

---
+++ configs/TrainerConfig.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TrainerConfig:
+    #
+    lr: float = 0.002
+    weight_decay: float = None
+    num_epochs: int = 10
+    batch_size: int = 1
+    validation_split: float = 0.2
+    test_split: float = 0.1
+    shuffle_dataset: bool = True
+    random_seed: int = 42
