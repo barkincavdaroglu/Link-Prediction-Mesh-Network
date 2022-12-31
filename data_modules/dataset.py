fixed vanishing gradients

data_modules/dataset.py
@@ -9,7 +9,7 @@ from utils.helpers import *
 class GraphDataset(torch.utils.data.Dataset):
     def __init__(
         self,
-        data_dir="data_processed",
+        data_dir="dataset_all_processed",
         mode="pickle",
         transform=None,
         target_transform=None,
@@ -17,14 +17,15 @@ class GraphDataset(torch.utils.data.Dataset):
         self.dir = data_dir
         self.mode = mode
         if mode == "pickle":
-            self.data = data_dir  # load_all_data(data_dir)
+            self.data = data_dir
         else:
             self.data = load_all_data(data_dir)
         self.transform = transform
+        self.seq_length = 9
         self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.data)
+        return int(len(os.listdir(self.dir)) / self.seq_length)
 
     def get_pickle(self, name):
         with open(name, "rb") as pickle_file:
@@ -32,7 +33,12 @@ class GraphDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         if self.mode == "pickle":
-            timeline = self.get_pickle(self.data + "/" + str(idx + 1) + ".pickle")
+            timeline = [
+                self.get_pickle(
+                    self.data + "/" + str(idx * self.seq_length + i) + ".pickle"
+                )
+                for i in range(self.seq_length)
+            ]
         else:
             timeline = self.data[idx]
         sample = timeline[:-1]

layers/EdgeUpdate.py
@@ -4,17 +4,17 @@ import torch.nn.functional as F
 
 
 class EdgeUpdate(torch.nn.Module):
-    def __init__(self, in_fts: int, edge_in_fts: int, out_dim: int) -> None:
+    def __init__(self, node_in_fts: int, edge_in_fts: int, edge_out_fts: int) -> None:
         super().__init__()
-        self.dim = out_dim
+        self.dim = edge_out_fts
         self.initial_update = torch.nn.Linear(
-            in_features=edge_in_fts, out_features=out_dim
+            in_features=edge_in_fts, out_features=edge_out_fts
         )
         self.mlp = torch.nn.Linear(
-            in_features=2 * in_fts + out_dim, out_features=out_dim
+            in_features=2 * node_in_fts + edge_out_fts, out_features=edge_out_fts
         )
         self.alpha_mlp = torch.nn.Linear(
-            in_features=2 * in_fts + out_dim, out_features=1
+            in_features=2 * node_in_fts + edge_out_fts, out_features=1
         )
 
     def forward(

layers/Generator.py
@@ -9,14 +9,24 @@ class Generator(nn.Module):
     def __init__(self, config: GeneratorConfig):
         """ """
         super(Generator, self).__init__()
-        node_in_w_head = (
-            (
-                config.num_heads_node * config.node_out_fts
+
+        if config.head_agg_mode == "concat":
+            node_in_w_head = (
+                (
+                    config.num_heads_node * config.node_out_fts
+                    + config.num_heads_node * config.edge_out_fts
+                )
+                * 2
+                if config.node_agg_mode == "concat"
+                else config.num_heads_node * config.node_out_fts
                 + config.num_heads_node * config.edge_out_fts
             )
-            if config.head_agg_mode == "concat"
-            else config.node_out_fts
-        )
+        else:
+            node_in_w_head = (
+                config.node_out_fts * 2
+                if config.node_agg_mode == "concat"
+                else config.node_out_fts
+            )
 
         self.node_num = config.node_num
 
@@ -32,27 +42,30 @@ class Generator(nn.Module):
             edge_out_fts=config.edge_out_fts,
             num_heads_node=config.num_heads_node,
             num_heads_graph=config.num_heads_graph,
+            num_nodes=config.node_num,
+            node_agg_mode=config.node_agg_mode,
             head_agg_mode=config.head_agg_mode,
+            nr_of_hops=config.nr_of_hops,
         )
+        self.hidden_dim = config.gru_hidden
 
-        self.gru = nn.GRU(
+        self.lstm = nn.LSTM(
             config.node_num * node_in_w_head,
             config.gru_hidden,
         )
 
-        """self.graph_gru = GraphGRU(
-            input_size_1=in_features,
-            hidden_size_1=in_features,
-            input_size_2=in_features * 2,
-            hidden_size_2=in_features,
-            input_size_3=in_features,
-            hidden_size_3=in_features,
+        self.leaky_relu = nn.LeakyReLU(0.2)
+
+        """self.gru = nn.GRU(
+            config.node_num * node_in_w_head,
+            config.gru_hidden,
         )"""
 
         self.ffn = nn.Sequential(
             nn.Linear(
                 config.gru_hidden,
-                int((config.node_num * config.node_num - config.node_num) / 2),
+                # int((config.node_num * config.node_num - config.node_num) / 2),
+                int(config.node_num * config.node_num),
             ),
             nn.Sigmoid(),
         )
@@ -66,14 +79,17 @@ class Generator(nn.Module):
         for input_ in inputs:
             edges, node_fts, edge_fts, graph_fts, adj = input_
 
-            agg_node_fts, agg_edge_fts, node_fts, edge_fts, edges = self.gn(
-                (node_fts, edge_fts, edges, adj)
-            )
+            _, _, node_fts, edge_fts, edges = self.gn((node_fts, edge_fts, edges, adj))
 
             all_node_fts = torch.cat((all_node_fts, node_fts.unsqueeze(0)), dim=0)
 
         all_node_fts = all_node_fts.view(-1, self.node_num * self.out_features)
-        _, hn = self.gru(all_node_fts)
+        h0 = torch.zeros(1, self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(1, self.hidden_dim).requires_grad_()
+
+        _, (hn, _) = self.lstm(all_node_fts, (h0.detach(), c0.detach()))
+
+        # hn = self.leaky_relu(hn)
 
         output = self.ffn(hn)
         return output

layers/NodeAttention.py
@@ -1,3 +1,4 @@
+from cmath import isnan
 from typing import List
 import torch
 import torch.nn as nn
@@ -12,7 +13,7 @@ class NodeAttentionHead(nn.Module):
         node_in_fts,
         node_out_fts,
         edge_in_fts,
-        edge_out_fts,
+        node_agg_mode,
         alpha=0.2,
         kernel_init=nn.init.xavier_uniform_,
         kernel_reg=None,
@@ -33,28 +34,22 @@ class NodeAttentionHead(nn.Module):
         self.node_in_fts = node_in_fts
         self.node_out_fts = node_out_fts
         self.edge_in_fts = edge_in_fts
-        self.edge_out_fts = edge_out_fts
+        self.node_agg_mode = node_agg_mode
 
         self.message_norm = MessageNorm(learn_scale=True)
 
         self.kernel_init = kernel_init
         self.kernel_reg = kernel_reg
 
-        self.W_node = nn.Parameter(torch.Tensor(node_in_fts, node_out_fts))
-        self.kernel_init(self.W_node)
-
+        self.W_node = nn.Linear(node_in_fts, node_out_fts)
         self.a_node = nn.Parameter(torch.Tensor(2 * node_out_fts + edge_in_fts, 1))
-        self.kernel_init(self.a_node, gain=1.414)
 
         self.leakyrelu = nn.LeakyReLU(alpha)
+        self._init_parameters()
 
-        self.node_update_agg = nn.Linear(node_out_fts, node_out_fts)
-
-    def reset_parameters(self):
-        self.kernel_init(self.W_node)
-        self.kernel_init(self.W_edge)
-        self.kernel_init(self.a_node)
-        self.kernel_init(self.a_edge)
+    def _init_parameters(self):
+        nn.init.xavier_uniform_(self.W_node.weight, gain=1.414)
+        nn.init.xavier_uniform_(self.a_node, gain=1.414)
 
     def forward(self, inputs: List[torch.Tensor]):
         """
@@ -72,7 +67,7 @@ class NodeAttentionHead(nn.Module):
         edges_undirected = torch.cat([edges, edges.flip(1)], dim=0)
 
         # Transform initial node features
-        h_v = torch.mm(node_fts, self.W_node)
+        h_v = self.W_node(node_fts)  # torch.mm(node_fts, self.W_node)
 
         # Each row will contain the tensor:
         # [node_fts[edge[0]], node_fts[edge[1]], edge_fts for edge]
@@ -89,7 +84,7 @@ class NodeAttentionHead(nn.Module):
         node_attention = self.leakyrelu(torch.matmul(h_v_expanded, self.a_node))
 
         # normalize attention scores using softmax
-        node_attention = torch.exp(torch.clamp(node_attention, -2, 2))
+        node_attention = torch.exp(node_attention)
 
         # For each node, sum the attention scores of all its neighbors
         node_attention_sum = unsorted_segment_sum(
@@ -98,12 +93,12 @@ class NodeAttentionHead(nn.Module):
             torch.unique(edges_undirected[:, 0]).shape[0],
         )
         # Repeat the sum for each neighbor of the node
-        node_attention_sum = node_attention_sum.repeat_interleave(
-            torch.bincount(edges_undirected[:, 0]), dim=0
-        )
-        # Normalize attention scores by dividing each by neighborhood sum
-        node_attention_norm = node_attention / node_attention_sum
+        node_attention_sum = node_attention_sum[edges_undirected[:, 0]]
 
+        # Normalize attention scores by dividing each by neighborhood sum
+        node_attention_norm = torch.log(node_attention / node_attention_sum)
+
+        # print("NORMALIZED ATT: ", node_attention_norm, "\n")
         node_attention_var = torch.var(node_attention_norm)
 
         # Get the node features of neighbors for all nodes
@@ -116,9 +111,15 @@ class NodeAttentionHead(nn.Module):
             torch.unique(edges_undirected[:, 0]).shape[0],
         )
 
-        # Normalize aggregated message
         agg_message_normalized = self.message_norm(h_v, agg_message)
 
-        final_embedding = self.node_update_agg(h_v + agg_message_normalized)
+        final_embed = (
+            torch.cat((h_v, agg_message_normalized), dim=1)
+            if self.node_agg_mode == "concat"
+            else h_v + agg_message_normalized
+        )
 
-        return final_embedding, node_attention_var
+        return (
+            final_embed,
+            node_attention_var,
+        )  # self.message_norm(h_v, agg_message), node_attention_var

utils/helpers.py
@@ -1,6 +1,7 @@
 import torch
 import networkx as nx
 import numpy as np
+from sklearn.preprocessing import PowerTransformer
 
 import os
 import pickle
@@ -50,30 +51,35 @@ def extract_node_features(G) -> torch.Tensor:
         print("Oops! Error calculating closeness_vitality for graph: ", G)
 
     barycenter_per_node = set(nx.barycenter(G))  # list
+
     degree_per_node = dict(G.degree())
+
     information_centrality_per_node = nx.information_centrality(
         G, weight="weight"
     )  # dict
+
     betweenness_centrality_per_node = nx.betweenness_centrality(
         G, weight="weight"
     )  # dict
-    articulation_points = set(nx.articulation_points(G))
+
+    # No aps in this dataset - disable for now
+    # articulation_points = set(nx.articulation_points(G))
 
     # Compute the average degree of the graph.
     avg_neighbor_degree = nx.average_neighbor_degree(G, weight="weight")
 
     # for each node, create a tensor of above features
     # return a tensor of shape (num_nodes, num_features)
-    node_fts = torch.zeros((len(G), 7))
+    node_fts = torch.zeros((len(G), 6))
 
     for i, node in enumerate(G.nodes()):
         node_fts[i, 0] = closeness_vit_per_node[node]
-        node_fts[i, 1] = node in barycenter_per_node
+        node_fts[i, 1] = int(node in barycenter_per_node)
         node_fts[i, 2] = degree_per_node[node]
         node_fts[i, 3] = information_centrality_per_node[node]
         node_fts[i, 4] = betweenness_centrality_per_node[node]
-        node_fts[i, 5] = node in articulation_points
-        node_fts[i, 6] = avg_neighbor_degree[node]
+        # node_fts[i, 5] = int(node in articulation_points)
+        node_fts[i, 5] = avg_neighbor_degree[node]
 
     return node_fts
 
@@ -153,47 +159,80 @@ def check_symmetric(a, rtol=1e-05, atol=1e-08):
     return np.allclose(a, a.T, rtol=rtol, atol=atol)
 
 
-def load_all_data(data_dir="dataset", mode="pickle"):
+def load_all_data(data_dir="dataset_all", mode="pickle"):
     samples = []
 
-    dirs = os.listdir(data_dir)
-    for subdir in dirs:
-        gs = []
+    all_node_fts = []
+    all_edge_fts = []
+    all_graph_fts = []
 
-        for filename in sorted(
-            os.listdir(os.path.join(data_dir, subdir)),
+    for i, filename in enumerate(
+        sorted(
+            os.listdir(data_dir),
             key=lambda x: int(x.split("_")[0]),
-        ):
-            # read .txt file and loop each line starting from third line
-            filename_dir = os.path.join(data_dir, subdir, filename)
+        )
+    ):
+        # read .txt file and loop each line starting from third line
+        filename_dir = os.path.join(data_dir, filename)
 
-            g = nx.read_edgelist(filename_dir, nodetype=int, data=(("weight", float),))
+        g = nx.read_edgelist(filename_dir, nodetype=int, data=(("weight", float),))
 
-            # for node in g.nodes():
-            #    g.add_edge(node, node, weight=0.0)
+        # for node in g.nodes():
+        #    g.add_edge(node, node, weight=0.0)
 
-            node_fts = extract_node_features(g)
-            edge_fts = extract_edge_features(g)
-            graph_fts = extract_graph_features(g)
-            adj_numpy = nx.to_numpy_matrix(g)
+        node_fts = extract_node_features(g)
+        all_node_fts.append(node_fts)
 
-            assert check_symmetric(adj_numpy)
+        edge_fts = extract_edge_features(g)
+        all_edge_fts.append(edge_fts)
 
-            adj = torch.tensor(adj_numpy, dtype=torch.float)
+        graph_fts = extract_graph_features(g)
+        all_graph_fts.append(graph_fts)
 
-            edges = torch.tensor([[e[0], e[1]] for e in g.edges()]).t().contiguous()
+        adj_numpy = nx.to_numpy_matrix(g)
 
-            g_tensor = [
-                edges,
-                node_fts,
-                edge_fts,
-                graph_fts,
-                adj,
-            ]
+        assert check_symmetric(adj_numpy)
 
-            gs.append(g_tensor)
-            # For faster loading, uncomment the block below to save the processed data as pickle files
-            # with open("data_processed/" + subdir + ".pickle", "wb") as ft_tensors:
-            #     pickle.dump(gs, ft_tensors)
-        samples.append(gs)
+        adj = torch.tensor(adj_numpy, dtype=torch.float)
+
+        edges = torch.tensor([[e[0], e[1]] for e in g.edges()]).t().contiguous()
+
+        g_tensor = [
+            edges,
+            node_fts,
+            edge_fts,
+            graph_fts,
+            adj,
+        ]
+
+        # gs.append(g_tensor)
+        # For faster loading, uncomment the block below to save the processed data as pickle files
+        # with open("dataset_all_processed/" + str(i) + ".pickle", "wb") as ft_tensors:
+        #    pickle.dump(g_tensor, ft_tensors)
+        samples.append(g_tensor)
+
+    pt_node_fts = PowerTransformer()
+    all_node_fts = torch.vstack(all_node_fts)
+    pt_node_fts.fit(all_node_fts)
+
+    pt_edge_fts = PowerTransformer()
+    all_edge_fts = torch.vstack(all_edge_fts)
+    pt_edge_fts.fit(all_edge_fts)
+
+    pt_graph_fts = PowerTransformer()
+    all_graph_fts = torch.vstack(all_graph_fts)
+    pt_graph_fts.fit(all_graph_fts)
+
+    for i in range(len(samples)):
+        samples[i][1] = torch.tensor(pt_node_fts.transform(samples[i][1])).float()
+        samples[i][2] = torch.tensor(pt_edge_fts.transform(samples[i][2])).float()
+        samples[i][3] = torch.tensor(
+            pt_graph_fts.transform(samples[i][3].reshape(1, -1))
+        ).float()
+        with open("dataset_all_processed/" + str(i) + ".pickle", "wb") as ft_tensors:
+            pickle.dump(samples[i], ft_tensors)
+
     return samples
+
+
+# load_all_data()