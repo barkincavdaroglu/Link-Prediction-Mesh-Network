2e05769dfc7516cee8df211dfa87ee1b8cbfabbd
BC <barkincavdar@gmail.com>
2023-01-02 17:29:01 +0300

    edit config dataclasses for varying epoch numbers of pretraining and gan training

--- configs/TrainerConfig.py
+++ configs/TrainerConfig.py
@@ -4,16 +4,18 @@ from dataclasses import dataclass, asdict
 @dataclass
 class TrainerConfig:
     # Learning rate
-    lr: float = 0.001  # 0.0025  # previously 0.0005
+    lr: float = 0.002  # 0.0025  # previously 0.0005
     # https://pytorch-lightning.readthedocs.io/en/stable/advanced/training_tricks.html#gradient-clipping
     is_clip_grads: bool = False
-    gradient_clip_val: float = 10.0
+    gradient_clip_val: float = 6.0
     gradient_clip_algorithm: str = "norm"
 
     track_grad_norm: int = 2
     # Weight decay / L2 Regularization
     weight_decay: float = 0.0005
-    num_epochs: int = 20
+    # num_epochs: int = 20
+    pretrain_epochs: int = 30
+    gan_epochs: int = 5
 
     def dict(self):
         return {k: str(v) for k, v in asdict(self).items()}
